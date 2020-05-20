8604b5917749880d937d2c1d31fc154352a73122
Anton Borisov <aborisov@yandex.ru>
2020-05-20 19:20:15 +0700

    Lesson 2 Prepare

---
+++ spring-mvc/src/main/java/ru/geekbrains/controller/UserController.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.geekbrains.persist.enity.User;
+import ru.geekbrains.persist.repo.UserRepository;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String userList(Model model) {
+        logger.info("User list");
+
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
+    }
+
+    @GetMapping("new")
+    public String createUser(Model model) {
+        logger.info("Create user form");
+
+        model.addAttribute("user", new User());
+        return "user";
+    }
+
+    @PostMapping
+    public String saveUser(User user) {
+        logger.info("Save user method");
+
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+}
---
+++ spring-mvc/src/main/java/ru/geekbrains/persist/repo/UserRepository.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.persist.repo;
+
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.persist.enity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class UserRepository {
+
+    private final AtomicLong identityGen;
+
+    private final Map<Long, User> users;
+
+    public UserRepository() {
+        this.identityGen = new AtomicLong(0);
+        this.users = new ConcurrentHashMap<>();
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    public void save(User user) {
+        long id = identityGen.incrementAndGet();
+        user.setId(id);
+        users.put(id, user);
+    }
+
+    public User findById(long id) {
+        return users.get(id);
+    }
+}
