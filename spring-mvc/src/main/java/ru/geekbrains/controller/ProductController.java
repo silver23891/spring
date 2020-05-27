75c7b0ef4aafae0c39e6bb9121e94e784d09b3e6
Anton Borisov <aborisov@yandex.ru>
2020-05-27 22:31:24 +0700

    Lesson 4

--- spring-mvc/src/main/java/ru/geekbrains/controller/ProductController.java
+++ spring-mvc/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -6,8 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.persist.entity.Product;
 import ru.geekbrains.persist.repo.ProductRepository;
+
+import java.math.BigDecimal;
 
 @RequestMapping("/product")
 @Controller
@@ -21,8 +24,20 @@ public class ProductController {
     }
 
     @GetMapping
-    public String listProducts(Model model) {
-        model.addAttribute("products", productRepository.findAll());
+    public String listProducts(
+            Model model,
+            @RequestParam(name = "minCost", required = false) BigDecimal minCost,
+            @RequestParam(name = "maxCost", required = false) BigDecimal maxCost
+    ) {
+        if (minCost == null && maxCost == null) {
+            model.addAttribute("products", productRepository.findAll());
+        } else if (minCost != null && maxCost == null) {
+            model.addAttribute("products", productRepository.findByCostGreaterThanEqual(minCost));
+        } else if (minCost == null && maxCost != null) {
+            model.addAttribute("products", productRepository.findByCostLessThanEqual(maxCost));
+        } else {
+            model.addAttribute("products", productRepository.findByCostBetween(minCost, maxCost));
+        }
         return "products";
     }
 
