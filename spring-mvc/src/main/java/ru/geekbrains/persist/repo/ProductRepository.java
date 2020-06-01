9d74d3d4718d301f758e8af2933b16cddbf59d56
Anton Borisov <aborisov@yandex.ru>
2020-06-01 18:33:16 +0700

    Lesson 5

--- spring-mvc/src/main/java/ru/geekbrains/persist/repo/ProductRepository.java
+++ spring-mvc/src/main/java/ru/geekbrains/persist/repo/ProductRepository.java
@@ -1,13 +1,16 @@
 package ru.geekbrains.persist.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.geekbrains.persist.entity.Product;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCostGreaterThanEqual(BigDecimal minCost);
-    List<Product> findByCostLessThanEqual(BigDecimal maxCost);
-    List<Product> findByCostBetween(BigDecimal minCost, BigDecimal maxCost);
+    Page<Product> findAll(Pageable pageable);
+    Page<Product> findByCostGreaterThanEqual(BigDecimal minCost, Pageable pageable);
+    Page<Product> findByCostLessThanEqual(BigDecimal maxCost, Pageable pageable);
+    Page<Product> findByCostBetween(BigDecimal minCost, BigDecimal maxCost, Pageable pageable);
+    Page<Product> findByTitleContaining(String title, Pageable pageable);
 }
